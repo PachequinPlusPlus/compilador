a5839f8dc0d081afdd98a0ff1c21fe4f0ee6e8d3
pachequinplusplus <samuel-1998@live.com>
2019-11-13 13:18:54 -0600

    removed unused functions

--- lex/semantic.py
+++ lex/semantic.py
@@ -30,23 +30,6 @@ class semantics:
         #print(json.dumps(parsed2, indent=4))
 
 
-    classes = {
-                "global" : {
-                    "attr" : [],
-                    "metodos" : {
-                          "main" : {
-                              "attr" : [],
-                              "tipo" : "void",
-                              "params" : 0,
-                              "isArray" : False,
-                              "size" : 0
-                           }
-                    },
-                    "parent" : None
-                    }
-            }
-
-
     #TODO(correguir las address temporales)
     def getAddress(self, tipo, isTemp = False):
         #TODO(que hacer para las clases?)
@@ -195,102 +178,12 @@ class semantics:
                         if atr.name == varName:
                             return atr
         return None
-
-
        
-
-    def setDefaultFunction(self, funcName):
-        scope = self.tope(self.scopes)
-        self.classes.setdefault(scope, {}).setdefault("metodos", {}).setdefault(funcName, {}).setdefault("attr", [])
-
-    def existInFunction(self, funcName, varName):
-        scope = self.tope(self.scopes)
-        self.setDefaultFunction(funcName)
-
-        tmp = self.classes.get(scope).get("metodos").get(funcName).get("attr")
-        for elem in tmp:
-            if elem["name"] == varName:
-                return True
-
-       # tmp = self.classes.get(scope).get("attr")
-       # for elem in tmp:
-       #     if elem["name"] == varName:
-       #         return True
-
-      #  parent = self.classes.get(scope).get("parent")
-
-#        while parent != None:
-#            tmp = self.classes.get(parent).get("attr")
-#            for elem in tmp:
-#                if elem["name"] == varName:
-#                    return True
-#            parent = self.classes.get(parent).get("parent")
-
-        return False
-
-    def setDefault(self, scope):
-        self.classes.setdefault(scope, {}).setdefault("attr", [])
-
-    def exists(self, varName):
-        scope = self.tope(self.scopes)
-        # set the default for the actual scope
-#        while scope != None:
-        self.setDefault(scope)
-        tmp = self.classes.get(scope).get("attr")
-        for elem in tmp:
-            if elem["name"] == varName:
-                return True
-        return False
-
-
-    def updateNumberParams(self, funcName, numberParams):
-        scope = self.tope(self.scopes)
-        self.classes.get(scope).get("metodos").get(funcName).setdefault("params", 0)
-        self.classes.get(scope).get("metodos").get(funcName)["params"] = numberParams
-
-    def updateReturnType(self, funcName, returnType):
-        scope = self.tope(self.scopes)
-        self.classes.get(scope).get("metodos").get(funcName).setdefault("tipo", 'void')
-        self.classes.get(scope).get("metodos").get(funcName)["tipo"] = returnType
-        if returnType != 'void':
-            self.addAttrFunction(funcName, "_"+funcName, returnType)
-
-
-
-    
-
-    def deepLookingRest(self, varName):
-        scope = self.tope(self.scopes)
-        self.setDefault(scope)
-
-        tmp = self.classes.get(scope).get("attr")
-        for elem in tmp:
-            if elem["name"] == varName:
-                return True
-
-        parent = self.classes.get(scope).get("parent")
-
-        while parent != None:
-            tmp = self.classes.get(parent).get("attr")
-            for elem in tmp:
-                if elem["name"] == varName:
-                    return True
-            parent = self.classes.get(parent).get("parent")
-
-        return False
-
     def tope(self, lista):
         if len(lista) == 0:
             return -1
         fr = lista[len(lista)-1]
         return fr
-
-    def addMethod(self, name):
-        clss = self.tope(self.scopes)
-        self.classes.setdefault(clss, {}).setdefault("metodos", {})[name] = {}
-
-    def addFunction(self, name):
-        self.classes.setdefault("global", {})["metodos"][name] = {}
 
     # new functions
     #-------------------------------------------------------------------------------
@@ -323,45 +216,14 @@ class semantics:
         func.appendVar(variable(name, tipo, direccion, isArray))
 
     #-------------------------------------------------------------------------------
-
-
-
-
-
-    def addAttrFunction(self, funcName, varName, tipo):
-        scope = self.tope(self.scopes)
-        addr = self.getAddress(tipo)
-
-        self.classes.setdefault(scope, {}).setdefault("metodos", {}).setdefault(funcName, {}).setdefault("attr", []).append({"name" : varName, "direccion" : addr, "tipo" : tipo})
-
-    def addAttrGlobal(self, varName, tipo):
-        scope = self.tope(self.scopes)
-        addr = self.getAddress(tipo)
-
-        self.classes.setdefault(scope, {}).setdefault("attr", []).append({"name" : varName, "direccion" : addr, "tipo" : tipo})
-
         
 
-    def appendType(self, tipo):
-        self.types.append(tipo)
 
-    def popType(self):
-        self.types.pop()
 
-    def appendParentForClasses(self, classe, parent):
-        self.classes.setdefault(classe, {}).setdefault("parent", parent)
 
-    def appendScope(self, scope):
-        self.scopes.append(scope)
 
-    def popScope(self):
-        self.scopes.pop()
 
-    def appendFuncs(self, funcName):
-        self.funcs.append(funcName)
 
-    def popFuncs(self):
-        self.funcs.pop()
 
     cube = {
             'int' : {
