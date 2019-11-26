3d811ec317cd2dd6717a7f3537fe35493f190d48
pachequinplusplus <samuel-1998@live.com>
2019-11-26 15:34:21 -0600

    fixed vm and now the fib and fact works

--- lex/virtualMachine.py
+++ lex/virtualMachine.py
@@ -59,17 +59,23 @@ class VM:
   def execute(self, quads):
     self.read_quadruples(quads)
     while(self.ip[len(self.ip)-1] != len(self.quadruples)):
+
+#      print(self.ip[len(self.ip)-1])   
+
       current_quad = self.quadruples[self.ip[len(self.ip)-1]]
       # Start mapping of operations
       if current_quad[0] == mappingQuads.MAS_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand + right_operand)
+
       elif current_quad[0] == mappingQuads.MENOS_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand - right_operand)
+
       elif current_quad[0] == mappingQuads.MULT_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand * right_operand)
+
       elif current_quad[0] == mappingQuads.DIV_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         if (right_operand == 0):
@@ -78,47 +84,61 @@ class VM:
           self.set_value(current_quad[3], left_operand // right_operand)
         else:
           self.set_value(current_quad[3], left_operand / right_operand)
+
       elif current_quad[0] == mappingQuads.AND_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand and right_operand)
+
       elif current_quad[0] == mappingQuads.OR_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand or right_operand)
+
       elif current_quad[0] == mappingQuads.DIFERENTE_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand != right_operand)
+
       elif current_quad[0] == mappingQuads.IGUALQUE_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand == right_operand)
+
       elif current_quad[0] == mappingQuads.MAYOR_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand > right_operand)
+
       elif current_quad[0] == mappingQuads.MAYORIGUAL_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand >= right_operand)
+
       elif current_quad[0] == mappingQuads.MENOR_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand < right_operand)
+
       elif current_quad[0] == mappingQuads.MENORIGUAL_I:
         [left_operand, right_operand] = self.convert_both(current_quad[1], current_quad[2])
         self.set_value(current_quad[3], left_operand <= right_operand)
+
       elif current_quad[0] == mappingQuads.GOTO_I:
         self.ip[len(self.ip)-1] = current_quad[3] - 1
+
       elif current_quad[0] == mappingQuads.GOTOF_I:
         left_operand = self.convert_left(current_quad[1])
         if not left_operand:
           self.ip[len(self.ip)-1] = current_quad[3] - 1
+
       elif current_quad[0] == mappingQuads.PARAM_I:
         left_operand = self.convert_left(current_quad[1])
         self.dict_params.append((current_quad[3], left_operand))
+
       elif current_quad[0] == mappingQuads.ERA_I:
-        # self.local_memory.append([[], [], []])
         pass
+
       elif current_quad[0] == mappingQuads.GOSUB_I:
         self.ip.append(current_quad[3] - 1)
         self.local_memory.append([[], [], []])
         for val in self.dict_params:
           self.set_value(val[0], val[1])
+        self.dict_params = []
+
       elif current_quad[0] == mappingQuads.ENDPROC_I:
         # get previous ip
         self.ip.pop()
@@ -127,13 +147,17 @@ class VM:
         # clear params
         self.params = {}
         self.offset.pop()
-        self.ip[len(self.ip)-1] = self.ip[len(self.ip)-1]-1      
+        self.ip[len(self.ip)-1] = self.ip[len(self.ip)-1]          
+
       elif current_quad[0] == mappingQuads.CI_I:
         self.set_value(current_quad[3], int(current_quad[2]))
+
       elif current_quad[0] == mappingQuads.CF_I:
         self.set_value(current_quad[3], float(current_quad[2]))
+
       elif current_quad[0] == mappingQuads.CC_I:
         self.set_value(current_quad[3], current_quad[2])
+
         # self.constant_memory[current_quad[3]/sizeMemory - constantOffset][current_quad[3]%sizeMemory-initialOffset] = str(current_quad[2])
       elif current_quad[0] == mappingQuads.PRINT_I:
           val =str(self.get_value(current_quad[3]))
@@ -160,6 +184,7 @@ class VM:
       elif current_quad[0] == mappingQuads.SUM_VAL_ADDRESS_I:
         left_operand = self.convert_left(current_quad[1])
         self.set_value(current_quad[3], left_operand + int(current_quad[2]))
+
       elif current_quad[0] == mappingQuads.IGUAL_I:
         right_operand = self.convert_right(current_quad[2])
         typeOp = self.get_type(current_quad[2])
@@ -168,6 +193,7 @@ class VM:
         elif (typeOp == "int"):
           right_operand = int(right_operand)
         self.set_value(current_quad[3], right_operand)
+
       elif current_quad[0] == mappingQuads.SET_I:
         right_operand = str(current_quad[2])
         if self.offset[len(self.offset)-1][1] != right_operand:
@@ -214,6 +240,7 @@ class VM:
     except:
       # TODO(CorrectError??)
       raise ValueError(f"Failed at parsing value={value}")
+
     if (memory_type == "global"):
       self.global_memory[direccion//sizeMemory][direccion%sizeMemory-initialOffset] = newValue
     elif (memory_type == "local"):
@@ -225,13 +252,13 @@ class VM:
     memory_type = self.get_memory_type(direccion) 
     default = self.get_default_type(direccion)
     if (memory_type == "global"):
-      for _ in range(direccion%sizeMemory-initialOffset+1):
+      while len(self.global_memory[direccion//sizeMemory]) < direccion%sizeMemory-initialOffset+1:
         self.global_memory[direccion//sizeMemory].append(default)
     elif (memory_type == "local"):
-      for _ in range(direccion%sizeMemory-initialOffset+1):
+      while len(self.local_memory[len(self.local_memory)-1][direccion//sizeMemory-localOffset]) < direccion%sizeMemory-initialOffset+1:
         self.local_memory[len(self.local_memory)-1][direccion//sizeMemory-localOffset].append(default)
     elif (memory_type == "constant"):
-      for _ in range(direccion%sizeMemory-initialOffset+1):
+      while(len(self.constant_memory[direccion//sizeMemory-constantOffset]) < direccion%sizeMemory-initialOffset+1):
         self.constant_memory[direccion//sizeMemory-constantOffset].append(default)
 
 
